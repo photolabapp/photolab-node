b8ad4a49c7919b03da40633fe8377b5dd246fc9b
MAURICIO MERLIN MACHADO <60003046@mauriciomachado.riachuelo.net>
2019-10-18 16:23:04 -0300

    fix route

--- app/route/routes.js
+++ app/route/routes.js
@@ -3,31 +3,14 @@ var shippingController = require('../controller/shippingController.js');
 var orderController = require('../controller/orderController.js');
 var orderPhotoController = require('../controller/orderPhotoController.js');
 var multer  = require('multer')
+var cors = require('cors');
 
 //var upload = multer({ storage: multer.memoryStorage({}) })
 var upload = multer({ dest: 'uploads/' })
 
 module.exports = function (app) {
     
-   app.use(function (req, res, next) {
-
-    	// Website you wish to allow to connect
-    	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
-
-    	// Request methods you wish to allow
-    	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    	// Request headers you wish to allow
-    	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    	// Set to true if you need the website to include cookies in the requests sent
-    	// to the API (e.g. in case you use sessions)
-    	res.setHeader('Access-Control-Allow-Credentials', true);
-
-    	// Pass to next layer of middleware
-      	next();
-    });    
-
+    app.use(cors({origin: 'http://localhost:8080'}));
     app.post('/user', userController.create);
     app.get('/user', userController.read);
     app.get('/user/id', userController.findById);
